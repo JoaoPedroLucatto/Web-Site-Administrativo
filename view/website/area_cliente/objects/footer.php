d33d4ffc00106459253810520de8be0a27533f0e
Reginaldo Savian Júnior <reginaldosavian@gmail.com>
2021-05-10 21:51:32 -0300

    Frontend Update
    - Ajuste dos contatos no footer

--- view/website/area_cliente/objects/footer.php
+++ view/website/area_cliente/objects/footer.php
@@ -12,9 +12,8 @@
     <div class="col s12 developer">
     	<span class="title"> Todos os direitos reservados </span>
 
-    	<a href="#" class="socialmedia"> <img src="images/icons/socialmedia/skype.png"> </a>
-    	<a href="#" class="socialmedia"> <img src="images/icons/socialmedia/facebook.png"> </a>
-    	<a href="#" class="socialmedia"> <img src="images/icons/socialmedia/linkedin.png"> </a>
-    	<a href="#" class="socialmedia"> <img src="images/icons/socialmedia/whatsapp.png"> </a>
+    	<a href="https://www.facebook.com/joaopedro.ayruthlucatto" class="socialmedia" target="_blank"> <img src="images/icons/socialmedia/facebook.png"> </a>
+        <a href="https://www.linkedin.com/in/reginaldo-savian-jr-045820141/" class="socialmedia" target="_blank"> <img src="images/icons/socialmedia/linkedin.png"> </a>
+        <a href="https://wa.me/5517997252265?text=" class="socialmedia" target="_blank"> <img src="images/icons/socialmedia/whatsapp.png"> </a>
     </div>
 </div>
